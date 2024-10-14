942c1e16b8205882f68ffb6c871c2bb678bd5047
Pétrus Pradella <petrus_ant@hotmail.com>
2024-10-14 15:27:36 -0300

    If we are on 1.20.x assume we are on 1.20.2

--- modules/Commons/src/main/java/br/com/finalcraft/bukkitpixelmonplaceholders/common/factory/PixelmonPlaceholderFactory.java
+++ modules/Commons/src/main/java/br/com/finalcraft/bukkitpixelmonplaceholders/common/factory/PixelmonPlaceholderFactory.java
@@ -3,14 +3,23 @@ package br.com.finalcraft.bukkitpixelmonplaceholders.common.factory;
 import br.com.finalcraft.evernifecore.config.playerdata.IPlayerData;
 import br.com.finalcraft.evernifecore.placeholder.replacer.RegexReplacer;
 import br.com.finalcraft.evernifecore.util.FCReflectionUtil;
+import br.com.finalcraft.evernifecore.version.MCDetailedVersion;
 import br.com.finalcraft.evernifecore.version.MCVersion;
 
 public abstract class PixelmonPlaceholderFactory<POKEMON> {
 
     public static PixelmonPlaceholderFactory<?> create(){
-        return (PixelmonPlaceholderFactory<?>) FCReflectionUtil.getConstructor(
-                "br.com.finalcraft.bukkitpixelmonplaceholders.compat." + MCVersion.getCurrent().name() +".reforged.factory.PixelmonPlaceholderFactoryImpl"
+
+        String versionName = MCVersion.getCurrent().name();
+        if (MCVersion.isEqual(MCVersion.v1_12)){
+            versionName = MCDetailedVersion.v1_12_R2.name();
+        }
+
+        PixelmonPlaceholderFactory<?> factory = (PixelmonPlaceholderFactory<?>) FCReflectionUtil.getConstructor(
+                "br.com.finalcraft.bukkitpixelmonplaceholders.compat." + versionName +".reforged.factory.PixelmonPlaceholderFactoryImpl"
         ).invoke();
+
+        return factory;
     }
 
     public abstract RegexReplacer<IPlayerData> getMainReplacer();
